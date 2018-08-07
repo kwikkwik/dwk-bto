c73c85da4c3d210560c341f08aa980c187eb676d
kwikkwik <41615346+kwikkwik@users.noreply.github.com>
2018-08-07 18:57:28 +0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,11 +4,11 @@ module.exports.run = async (bot, message, args, ops, PREFIX) => {
   let postMsg = await message.channel.send('**Please Wait...**');
   message.channel.startTyping();
  const embed = new Discord.RichEmbed()
-    .setColor(0xf44336)
+    .setColor(0x00d303)
     .setTitle("Invite | Support:") 
-    .setDescription("[Invite me](https://discordapp.com/oauth2/authorize?client_id=452360666020577281&scope=bot&permissions=2012593399) | [Vote me](https://discordbots.org/bot/452360666020577281/vote) | [Support server](https://discord.gg/kDAYc8M)")    
+    .setDescription("[Invite me](https://discordapp.com/oauth2/authorize?client_id=473444210914099204&scope=bot&permissions=2146958591) | [Vote me](https://discordbots.org/bot/452360666020577281/vote) | [Support server](https://discord.gg/kDAYc8M)")    
     .addField(`Found any bug?`, `Usage: ${PREFIX}bug <specify a bug>`) 
-    .setFooter("Thanks for support | Created By Sharif#2769");
+    .setFooter("Thanks for support | Created By _Brickmaster_#0218");
   setTimeout(() => {
 postMsg.edit(embed)
 }, 1000);
