94d533632ea403ecb48610fca2ad8518bd1c5d31
kwikkwik <41615346+kwikkwik@users.noreply.github.com>
2018-08-11 19:14:39 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,13 +5,12 @@ module.exports.run = async (bot, message, args, ops, PREFIX) => {
 let embed = new Discord.RichEmbed()
 .setColor('RANDOM')
 .setTitle('List of all commands') 
-.addField('General Commands', '\`help\` \`helphere\` \`botinfo\` \`bug\` \`userinfo\` \`serverinfo\` \`stats\` \`ping\` \`report\` \`invite\`')
-.addField('Music', '\`play\` \`pause\` \`resume\` \`skip\` \`stop\` \`np\` \`queue\` \`volume\`')
+.addField('General Commands', '\`help\` \`botinfo\` \`bug\` \`userinfo\` \`serverinfo\` \`stats\` \`ping\` \`report\` \`invite\`')
 .addField('Image', '\`slap\` \`avatar\` \`yesorno\` \`meme\` \`bunny\` \`kiss\` \`hug\` \`gif\` \`pat\`')
 .addField('Utility and Fun', '\`hastebin\` \`calculator\` \`rps\` \`discrim\` \`fmk\` \`asciify\` \`embed\` \`emojify\` \`ask\` \`say\` \`weather\`')
 .addField('Moderation', '\`ban\` \`kick\` \`mute\` \`unmute\` \`warn\` \`esay\` \`purge\`')
 .addField('N.S.F.W', '\`urban\` \`hentaigif\` \`lewd\` \`nekos\` \`xxx\` \`urban\`')
-.setFooter('© Sharif#2769');
+.setFooter('© DwKBot');
 message.author.send(embed);
 
 message.channel.send(`${message.author}, 📬 Sending help to your DM! If you don't have any Dm's from me please do ${PREFIX}helphere`);
