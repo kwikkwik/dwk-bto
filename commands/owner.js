7c9e38254998b1a9458f00580c6f30262d820812
kwikkwik <41615346+kwikkwik@users.noreply.github.com>
2018-08-11 18:21:57 +0700

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message, args) => {
-  message.channel.send(`My Owner is <@!444454206800396309> ,  his named _**Sharif**_ and I love _**Sharif**_, Even though he made me use the makeshift equipment, but I stayed with him. and I thank the people who taught and helped him to make me pretty good...\n__**Here is the list name people help Sharif**__\n-**Hazmi35#1855**,\n-**Aηdiη.#9544**,\n-**12042#5754**,\n-**Kina#9305**,\n-**ForceStop#4120**.`)
+  message.channel.send(`My Owner is <@!335035386923581440> ,  his named _**Brickmaster**_ `)
   };
 
 module.exports.help = { 
