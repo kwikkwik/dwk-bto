a7ed2a0a36b9fc67900d136db5e87a78032ca450
kwikkwik <41615346+kwikkwik@users.noreply.github.com>
2018-08-15 14:49:01 +0700

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -26,18 +26,18 @@ module.exports.run = (bot, message, args) => {
         .setColor('RANDOM')
         .setAuthor("User Info", message.author.displayAvatarURL)
         .addBlankField()
-        .addField("Name:", `${user.tag}`, true)
+        .addField("Name:", `\`${user.tag}\``, true)
         .addField("ID:", `${user.id}`, true)
         .addField("Account Type:", `${trufal[user.bot]}`, true)
         .addField("Status:", `${status[user.presence.status]}`, true)
         .addField("Game:", `${user.presence.game ? user.presence.game.name : 'I do not see him playing anything!'}`, true)
-        .addField("Roles:", member.roles.map(roles => `${roles}`).join(' => '), true)
+        .addField("Roles:", member.roles.map(roles => `${roles}`).join(' > '), true)
         .addField("Joined At:", message.member.joinedAt)
         .addField("Created At:", `${user.createdAt}`, true)
         .addBlankField()
         .setThumbnail(user.avatarURL)
         .setTimestamp()
-        .setFooter(`© DwKBot |`);
+        .setFooter(`© DwKBot `);
      message.channel.send({embed});
 };
 
