3969dc4e9bb190d0ced69e2e9868d92c9b4da635
kwikkwik <41615346+kwikkwik@users.noreply.github.com>
2018-08-15 13:22:26 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,15 +5,13 @@ module.exports.run = async (bot, message, args, ops, PREFIX) => {
 let embed = new Discord.RichEmbed()
 .setColor('RANDOM')
 .setTitle('List of all commands') 
-.addField('General Commands', '\`help\` \`botinfo\` \`bug\` \`userinfo\` \`serverinfo\` \`stats\` \`ping\` \`report\` \`invite\`')
+.addField('General Commands', '\`help\` \`bug\` \`userinfo\` \`serverinfo\` \`stats\` \`ping\` \`report\` \`invite\`')
 .addField('Image', '\`slap\` \`avatar\` \`yesorno\` \`meme\` \`bunny\` \`kiss\` \`hug\` \`gif\` \`pat\`')
 .addField('Utility and Fun', '\`hastebin\` \`calculator\` \`rps\` \`discrim\` \`fmk\` \`asciify\` \`embed\` \`emojify\` \`ask\` \`say\` \`weather\`')
 .addField('Moderation', '\`ban\` \`kick\` \`mute\` \`unmute\` \`warn\` \`esay\` \`purge\`')
 .addField('N.S.F.W', '\`urban\` \`hentaigif\` \`lewd\` \`nekos\` \`xxx\` \`urban\`')
 .setFooter('© DwKBot');
-message.author.send(embed);
-
-message.channel.send(`${message.author}, 📬 Sending help to your DM!`);
+message.channel.send(embed);
 
  }; 
 
