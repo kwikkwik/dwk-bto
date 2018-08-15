94bc3f675a8a4d083c72d56bd82cc1222e2adbc8
kwikkwik <41615346+kwikkwik@users.noreply.github.com>
2018-08-15 13:59:18 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const { ShardingManager } = require('discord.js');
+const shard = new ShardingManager('./bot.js',{
+  totalShards: 1,
+  token: process.env.TOKEN
+});
+
+shard.on('launch', pecahan => {
+  console.log(`💎Launching Shard ${pecahan.id} [ ${pecahan.id + 1} of ${shard.totalShards} ]`);
+});
+shard.on('message', (pecahan, msg) => {
+  console.log(`[${new Date().toString().split(" ", 5).join(" ")}] #${pecahan.id} | ${msg._eval} | ${pecahan._result}`);
+});
+shard.spawn();
+
+//require('./server.js');
+
+process.on('unhandledRejection', e => console.error(e))
+.on('uncaughtException', e => console.error(e));
