5cd88440dc917e1b7486076d1a332dd0cf4c8a6e
kwikkwik <41615346+kwikkwik@users.noreply.github.com>
2018-08-31 23:45:28 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,15 @@
 exports.run = async (bot) => {
+      let guilds = bot.shard ? await bot.shard.broadcastEval('this.guilds.size') : bot.guilds.size;
+    if (guilds instanceof Array) {
+      guilds = guilds.reduce((sum, val) => sum + val, 0);
+    }
+        let users = bot.shard ? await bot.shard.broadcastEval('this.users.size') : bot.users.size;
+    if (users instanceof Array) {
+      users = users.reduce((sum, val) => sum + val, 0);
+    }
   bot.user.setActivity('WATCHING');
   setInterval(() => {
-    let status = [`Master Bot`]
+    let status = [`Master Bot`,`${guilds} Guilds!`,`${users} Users!`]
     let random = Math.floor(Math.random() * status.length)
     bot.user.setActivity(status[random]);
   }, 20000);
